c7b72ac1cd7d27982cd87af61b39c7e8d9248482
Liam MacPherson <liam.macpherson@digital.homeoffice.gov.uk>
2023-12-13 16:00:00 +0000

    Update helper to remove error

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,11 +1,14 @@
-import { ActionInput, ActionInputParam } from './types';
+import {
+    ActionInput,
+    ActionInputParam
+} from './types';
 import * as core from '@actions/core';
 
 export const getActionInputs = (variables: Array<ActionInputParam>): ActionInput => {
     return <ActionInput>variables.reduce((obj, variable) => {
         let value: string | undefined = core.getInput(variable.name, variable.options);
         if (!value) {
-            if (Object.prototype.hasOwnProperty.call(variable, 'default')) {
+            if (Object.hasOwn(variable, 'default')) {
                 value = variable.default;
             }
         }
